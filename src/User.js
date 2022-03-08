671b60ab63f85ee39a2e747fb11817a18565fb18
anushka <anushkapragati999@gmail.com>
2022-03-08 13:51:51 +0530

    epan changes

--- src/User.js
+++ src/User.js
@@ -1,32 +1,68 @@
 import React from 'react'
-import {useState,useEffect} from 'react'
+import {useState,useEffect,useRef} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom';
 import './user.css'
 
 
 function User() {
+  const [searchResult, setsearchResult] = useState([])
+  const inputEl = useRef("");
   const [users, setusers] = useState([])
-  useEffect(() => {
-   loadusers();
+  const [Searchterm, setSearchterm] = useState('')
 
-   
+  useEffect(() => {
+   loadusers();   
   }, [])
+
   const loadusers = async () =>{
     const result = await axios.get("http://localhost:8081/users");
     // console.log(result);
     setusers(result.data.reverse())
+    setsearchResult(users);
     
+  }
+
+  const searchHandler=()=>{
+      // console.log(inputEl.current.value)
+      setSearchterm(inputEl.current.value);
+      if(Searchterm === " "  ){
+        setsearchResult(users);
+      }
+      else if(Searchterm !== "" ){
+        const newuserList= users.filter((val)=>{
+        return  Object.values(val)
+         .join(" ")
+         .toLowerCase()
+         .includes(Searchterm.toLowerCase());
+        });
+        setsearchResult(newuserList);
+      }      
+      else{
+        setsearchResult(users);
+      }
+     
+      console.log(searchResult)
+
   }
   const deleteUser = async id => {
     await axios.delete(`http://localhost:8081/users/${id}`);
     loadusers();
   }
+
   return (
-    <div className="container text-center mt-4">
+    <div className="container text-center mt-4">    
     <div className="user-heading ">
       <h1>Users</h1>
-      
+      <div className="search-user mb-3">
+      <input
+      ref={inputEl}
+       type="text" 
+      className='prompt' 
+      value={Searchterm}
+      onChange={searchHandler} placeholder='Search...' />
+      <i className="search icon"></i>        
+    </div>
     </div>
     
     <div className="display-users">
@@ -40,8 +76,8 @@ function User() {
     </tr>
   </thead>
   <tbody>
-    {
-      users.map((user,index) => (
+    {     
+      searchResult.map((user,index) => (
       <tr>
         <th scope="row">{index+1}</th>       
         <td>{user.username}</td>
